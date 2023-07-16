f06f00211b465d1ab51d0d005692883f07c238b9
SpaceChae <13278955+enaenen@users.noreply.github.com>
2023-07-17 03:06:27 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -27,7 +27,7 @@ const CONFIG = {
   link: "https://developspace.vercel.app",
   since: 2023, // If leave this empty, current year will be used.
   lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: "/avatar.svg", // The link to generate OG image, don't end with a slash
   seo: {
     keywords: ["Blog", "Website", "Notion"],
   },
